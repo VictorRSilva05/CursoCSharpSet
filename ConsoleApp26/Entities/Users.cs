0da17f945a34c3ea9efaa83e64914d93cf0206b1
Victor R. Silva <victorr.silva005@gmail.com>
2024-03-01 08:52:58 -0300

    Created file input and User class

---
+++ ConsoleApp26/Entities/Users.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp26.Entities
+{
+    internal class Users
+    {
+        public string Username { get; set; }
+        public DateTime AccessDate { get; set; }
+
+        public Users(string username, DateTime accessDate)
+        {
+            Username = username;
+            AccessDate = accessDate;
+        }
+    }
+}
---
+++ ConsoleApp26/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace ConsoleApp26
+{
+    internal class Program
+    {
+        Program()
+        {
+            Console.Write("Enter file full path: "); //C:\Windows\Temp\online_users.txt
+            string path = Console.ReadLine(); 
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string line = sr.ReadLine();
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+        static void Main(string[] args)
+        {
+            _ = new Program();
+        }
+    }
+}
